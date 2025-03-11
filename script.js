f0aae8fe75a2d8c8f798b25fb1cbb0c336b8a93e
DinisAzPereira <dinisazpereira@gmail.com>
2025-03-11 11:12:52 +0000

    Updating changeSquareBackground to randomcolor

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//loop that create 256 squares
+
+
 
 function SquaresNumberPromp() {
   var Prompt = prompt("Please enter grid size that you want:");
@@ -10,16 +11,24 @@ function SquaresNumberPromp() {
 
 
   } else {
+
+    const sideLenghtSquare = 320 / Prompt;
+
     for (let i = 0; i < Prompt * Prompt; i++) {
       const container = document.querySelector(".container");
-    
       const div = document.createElement("div");
       container.appendChild(div);
       div.classList.add("square");
+
+
+      div.style.height = sideLenghtSquare + "px";
+      div.style.width = sideLenghtSquare + "px"
+      div.style.border = "0.5px solid black"
+
     }
       changeSquareBackground()
   }
-
+    
 }
 
 
@@ -27,12 +36,13 @@ function changeSquareBackground() {
 
   const list = document.querySelectorAll('.square');
 
+  const randomColor = Math.floor(Math.random()*16777215).toString(16);
 
 
   list.forEach(el => {
-    el.addEventListener('click', (e) => {
+    el.addEventListener('mouseover', (e) => {
         //code that affects the element you click on
-        el.style.backgroundColor = "#a52a2a"
+        el.style.backgroundColor = "#" + randomColor
        
     });
   }); 
